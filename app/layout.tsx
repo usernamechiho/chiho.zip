1cc0725b3d81420826269f45071332f0585c52fa
이치호 <minepanorama@naver.com>
2023-09-22 02:52:56 +0900

    FEAT: 컴포넌트 폴더 추가, 기본적인 레이아웃 세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,22 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import Header from "@/component/Header";
+import "./reset.css";
+import Navigation from "@/component/Navigation";
+import Layout from "@/component/Layout";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [hello, setHello] = useState("");
-
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+
+        <Navigation />
+
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
