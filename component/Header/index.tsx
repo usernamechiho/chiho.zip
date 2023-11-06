66565ed70c560f7d5d67e241d7acc69ba71bd524
이치호 <minepanorama@naver.com>
2023-11-06 23:27:57 +0900

    MOD: 바벨 삭제, 셋팅 수정

--- component/Header/index.tsx
+++ component/Header/index.tsx
@@ -1,3 +1,15 @@
+import { css } from "@emotion/react";
+
+const color = "red";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header
+      css={css`
+        color: ${color};
+      `}
+    >
+      Header
+    </header>
+  );
 }
