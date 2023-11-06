5b92e28e006173d77588ebc1cf6008efa74c57de
이치호 <minepanorama@naver.com>
2023-11-06 21:47:03 +0900

    ADD: 폰트(Pretendard) 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,28 @@
 import Header from "@/component/Header";
-import "./reset.css";
+import "./_styles/reset/reset.css";
 import Navigation from "@/component/Navigation";
 import Layout from "@/component/Layout";
+import localFont from "next/font/local";
+
+const customFont = localFont({
+  src: [
+    {
+      path: "./_styles/font/Pretendard-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./_styles/font/Pretendard-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./_styles/font/Pretendard-Bold.woff2",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={customFont.className}>
       <body>
         <Header />
 
